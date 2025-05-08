74b52923d08c0d318029856d7ad645092f5342dd
rnakb <r-nakamoto@glad-cube.com>
2025-05-08 17:21:32 +0900

    Sticky&Flex追加

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var param;
+
+if(!param){
+    param = "";
+    }
+if (param){
+    console.log( "paramあり" ); 
+    console.log( "paramの値は" + param );
+}else{ 
+    console.log( "paramなし" ); 
+    console.log( "paramの値は" + param ); 
+}
+
+        
